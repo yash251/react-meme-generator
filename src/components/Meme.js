bf739eabafe88638845c3a0013c2dab2c07566d1
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-26 16:51:46 +0530

    main comp raw layout

---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+function Meme() {
+    return (
+        <main>
+            <form>
+                <input type="text" />
+                <input type="text" />
+                <button>Get a new meme image 🖼</button>
+            </form>
+        </main>
+    );
+  }
+    
+  export default Meme;
