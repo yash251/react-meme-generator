a572a503852be87874aa0fd02563e530438d5e3a
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-26 16:53:47 +0530

    added classNames

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,10 +1,22 @@
 function Meme() {
     return (
         <main>
-            <form>
-                <input type="text" />
-                <input type="text" />
-                <button>Get a new meme image 🖼</button>
+            <form className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                >
+                    Get a new meme image 🖼
+                </button>
             </form>
         </main>
     );
