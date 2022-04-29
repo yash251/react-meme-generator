2aa8045de844a78c4c2f5c2a8607712e25fbf18d
Yash Jagtap <jagtapyash2512@gmail.com>
2022-04-29 05:38:42 +0530

    callback in useState

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -6,7 +6,15 @@ function Meme() {
     // const [isImportant, func] = React.useState("Yes")
     // console.log(isImportant)
     // output = "Yes"
-    
+    // Note: if you ever need the old value of state
+    // to help you determine the new value of state,
+    // you should pass a callback function to your
+    // state setter function instead of using
+    // state directly. This callback function will
+    // receive the old value of state as its parameter,
+    // which you can then use to determine your new
+    // value of state.
+
     return (
         <main>
             <form className="form">
