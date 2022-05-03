ced5573733a741494ed2ab596887095cea4e7f7b
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-03 22:50:31 +0530

    onChange, handleChange, name,value

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -21,6 +21,15 @@ function Meme() {
         }))
         
     }
+
+    function handleChange(event) {
+        const {name, value} = event.target;
+        setMeme(prevMeme =>({
+            ...prevMeme,
+            [name] : value
+        }))
+    } 
+
     // const result = React.useState("Yes")
     // console.log(result)
     // output = ["Yes", ƒ()]
@@ -44,11 +53,17 @@ function Meme() {
                     type="text"
                     placeholder="Top text"
                     className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
                 />
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
                 />
                 <button 
                     className="form--button"
@@ -57,7 +72,11 @@ function Meme() {
                     Get a new meme image 🖼
                 </button>
             </div>
-            <img src={meme.randomImage} className="meme--image" />
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
         </main>
     );
   }
