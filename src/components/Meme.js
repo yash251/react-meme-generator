cca8aebd8e61d41a75885f9e7f7a7743448d76c5
Yash Jagtap <jagtapyash2512@gmail.com>
2022-05-04 20:46:30 +0530

    useEffect takeaway

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -57,6 +57,14 @@ function Meme() {
     // which you can then use to determine your new
     // value of state.
 
+    // useEffect takes a function as its parameter. If that function
+    // returns something, it needs to be a cleanup function. Otherwise,
+    // it should return nothing. If we make it an async function, it
+    // automatically retuns a promise instead of a function or nothing.
+    // Therefore, if you want to use async operations inside of useEffect,
+    // you need to define the function separately inside of the callback
+    // function
+
     return (
         <main>
             <div className="form">
